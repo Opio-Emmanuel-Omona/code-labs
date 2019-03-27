c9517670014bfd4019aa75bac3bc5d557f48418f
Emmanuel Omona <emmanuel.omona@andela.com>
2019-03-27 17:36:57 +0300

    feat(retrofitInstance): create a retrofit instance and define API endpoints
    - create a service package
    - add GithubApi interface to service package
    - create a GithubServive to create a retrofit instance
    
    [Maintains #155890684]

---
+++ app/src/main/java/com/example/codelabs/service/GithubService.java
@@ -0,0 +1,24 @@
+package com.example.codelabs.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class GithubService {
+
+    private Retrofit retrofit = null;
+
+    public GithubApi getAPI() {
+        String BASE_URL = "https://api.github.com/";
+
+        if (retrofit == null) {
+            retrofit = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(GithubApi.class);
+    }
+}
